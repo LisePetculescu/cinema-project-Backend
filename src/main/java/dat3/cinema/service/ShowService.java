bdc10da3dfec75f06166f810c142f6b120169a8b
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-03-18 10:07:36 +0100

    get, put, post routes done but not testet

--- src/main/java/dat3/cinema/service/ShowService.java
+++ src/main/java/dat3/cinema/service/ShowService.java
@@ -1,7 +1,14 @@
 package dat3.cinema.service;
 
+import dat3.cinema.entity.Show;
 import dat3.cinema.repository.ShowRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class ShowService {
@@ -11,4 +18,25 @@ public class ShowService {
     public ShowService(ShowRepository showRepository) {
         this.showRepository = showRepository;
     }
+
+    public List<Show> getAllShows() {
+        return showRepository.findAll();
+    }
+
+    public ResponseEntity<Show> getOneShow( int id) {
+        return ResponseEntity.of(showRepository.findById(id));
+    }
+
+    public Show createShow(Show request) {
+        Show show = new Show();
+        return showRepository.save(show);
+    }
+
+    public Show updateShow(Show request, int id) {
+        Show showToEdit = showRepository.findById(id).orElseThrow(()
+                -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Show not found"));
+        return showRepository.save(showToEdit);
+    }
+
+
 }
