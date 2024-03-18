fc2d437c64aa7c1d7a80d4ae87a41f6098e7c786
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-03-18 13:03:08 +0100

    all tests done and ready for go

--- src/main/java/dat3/cinema/service/ShowService.java
+++ src/main/java/dat3/cinema/service/ShowService.java
@@ -1,6 +1,8 @@
 package dat3.cinema.service;
 
+import dat3.cinema.entity.Movie;
 import dat3.cinema.entity.Show;
+import dat3.cinema.entity.Theatre;
 import dat3.cinema.repository.ShowRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ public class ShowService {
 
     private ShowRepository showRepository;
 
+
     public ShowService(ShowRepository showRepository) {
         this.showRepository = showRepository;
     }
@@ -29,17 +32,26 @@ public class ShowService {
         return ResponseEntity.of(showRepository.findById(id));
     }
 
+// create a new show and return the created show object and throw a error if the show is not created
     public Show createShow(Show request) {
         Show show = new Show();
+        updateShow(show, request);
         return showRepository.save(show);
     }
 
+
+
+// update a show  with a given id and return the updated show object or throw a 404 error if the show is not found in the database
     public Show updateShow(Show request, int id) {
         Show showToEdit = showRepository.findById(id).orElseThrow(()
                 -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Show not found, can't update"));
+        showToEdit.setMovie(request.getMovie());
+        showToEdit.setTheatre(request.getTheatre());
+        showToEdit.setStartTime(request.getStartTime());
         return showRepository.save(showToEdit);
     }
 
+    // delete a show with a given id and return the deleted show object or throw a 404 error if the show is not found in the database
     public ResponseEntity<Show> deleteOneShow(int id) {
         Optional<Show> showToDelete = Optional.ofNullable(showRepository.findById(id).orElseThrow(()
                 -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Show not found, can't delete")));
@@ -47,4 +59,10 @@ public class ShowService {
         return ResponseEntity.of(showToDelete);
     }
 
+    private void updateShow(Show show, Show request) {
+        show.setMovie(request.getMovie());
+        show.setTheatre(request.getTheatre());
+        show.setDate(request.getDate());
+        show.setStartTime(request.getStartTime());
+    }
 }
