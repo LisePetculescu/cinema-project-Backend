068fc972ea5b29d9f3efddae2bbdfdb7483e4717
Malte <maltebjoerklund@gmail.com>
2024-03-19 15:31:55 +0100

    Update ReservationController.java

--- src/main/java/dat3/cinema/api/ReservationController.java
+++ src/main/java/dat3/cinema/api/ReservationController.java
@@ -2,8 +2,11 @@ package dat3.cinema.api;
 
 import dat3.cinema.dto.ReservationDto;
 import dat3.cinema.entity.Reservation;
+import dat3.cinema.repository.ReservationRepository;
 import dat3.cinema.service.ReservationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -13,14 +16,23 @@ import java.util.List;
 public class ReservationController {
 
     ReservationService reservationService;
+    ReservationRepository reservationRepository;
 
-    public ReservationController(ReservationService reservationService){
+    public ReservationController(ReservationService reservationService, ReservationRepository reservationRepository){
         this.reservationService = reservationService;
+        this.reservationRepository = reservationRepository;
     }
-    
+
     @GetMapping
     public List<ReservationDto> getAllReservations(){
         return reservationService.getAllReservations();
+    }
+
+    @GetMapping(path = "/id")
+    public ReservationDto getReservationById(int id){
+        Reservation reservation = reservationRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Reservation not found in database"));
+        return new ReservationDto(reservation);
     }
 
     @PostMapping
