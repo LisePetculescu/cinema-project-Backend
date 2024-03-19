d30aa33f1f331acb294275989656980e1574ec03
Bransholm <kenneth@bransholm.dk>
2024-03-19 02:30:13 +0100

    Movies CRUD routes created (waiting for test)

---
+++ src/main/java/dat3/cinema/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package dat3.cinema.repository;
+
+import dat3.cinema.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+}
