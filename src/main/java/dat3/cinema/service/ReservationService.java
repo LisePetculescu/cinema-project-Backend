575d4bc3fc66a855e6286f55e3474378fc01ab8f
Malte <maltebjoerklund@gmail.com>
2024-03-19 15:28:38 +0100

    Update ReservationService.java

--- src/main/java/dat3/cinema/service/ReservationService.java
+++ src/main/java/dat3/cinema/service/ReservationService.java
@@ -7,8 +7,13 @@ import dat3.cinema.entity.Show;
 import dat3.cinema.repository.CustomerRepository;
 import dat3.cinema.repository.ReservationRepository;
 import dat3.cinema.repository.ShowRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,6 +28,16 @@ public class ReservationService {
         this.reservationRepository = reservationRepository;
         this.showRepository = showRepository;
         this.customerRepository = customerRepository;
+    }
+
+    public List<ReservationDto> getAllReservations(){
+        return reservationRepository.findAll().stream().map(ReservationDto::new).toList();
+    }
+
+    public ReservationDto getReservationById(int id){
+        Reservation reservation = reservationRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Reservation not found in database");
+        return new ReservationDto(reservation);
     }
 
     public ReservationDto createReservation(ReservationDto request){
@@ -50,5 +65,24 @@ public class ReservationService {
         } else {
             original.setCustomer(null);
         }
+    }
+
+
+    public ReservationDto editReservation(ReservationDto request, int id){
+        Reservation reservationToEdit = reservationRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Reservation not found in database")
+        );
+        updateReservation(reservationToEdit, request);
+        reservationRepository.save(reservationToEdit);
+        return new ReservationDto(reservationToEdit);
+
+    }
+
+    public ResponseEntity<Void> deleteReservation(int id) {
+        Reservation reservationToDelete = reservationRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Reservation not found in database")
+        );
+        reservationRepository.delete(reservationToDelete);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
