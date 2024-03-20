6e7effa535440cc621047f08830be92d0b894527
Bransholm <kenneth@bransholm.dk>
2024-03-21 00:02:46 +0100

    PEC-43 3D changed to threeD. All tested and works

--- src/main/java/dat3/cinema/entity/Movie.java
+++ src/main/java/dat3/cinema/entity/Movie.java
@@ -21,16 +21,16 @@ public class Movie {
     private String actors;
     private int duration;
     private String genre;
-    private boolean is3D;
+    private boolean threeD;
     private boolean isActive;
 
-    public Movie(String title, String description, String actors, int duration, String genre, boolean is3D, boolean isActive) {
+    public Movie(String title, String description, String actors, int duration, String genre, boolean threeD, boolean isActive) {
         this.title = title;
         this.description = description;
         this.actors = actors;
         this.duration = duration;
         this.genre = genre;
-        this.is3D = is3D;
+        this.threeD = threeD;
         this.isActive = isActive;
     }
 }
