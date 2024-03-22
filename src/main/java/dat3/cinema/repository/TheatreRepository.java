be1e257a0065d54fa1708d27d9299f4f45439c3d
Bransholm <kenneth@bransholm.dk>
2024-03-22 13:17:01 +0100

    Theathe Backend Removed because it's already done

--- src/main/java/dat3/cinema/repository/TheatreRepository.java
+++
@@ -1,7 +0,0 @@
-package dat3.cinema.repository;
-
-import dat3.cinema.entity.Theatre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TheatreRepository extends JpaRepository<Theatre, Integer> {
-}
